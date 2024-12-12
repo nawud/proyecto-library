cce613a4bf3b341396836866ee624ce42dd351a5
nawud <duwan17@gmail.com>
2024-12-12 13:19:30 +0100

    proyecto-library

--- src/main/java/com/liberia/Library/controller/BookController.java
+++ src/main/java/com/liberia/Library/controller/BookController.java
@@ -2,9 +2,13 @@ package com.liberia.Library.controller;
 
 import com.liberia.Library.model.Book;
 import com.liberia.Library.service.BookService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -30,5 +34,24 @@ public class BookController {
     public void deleteBookByid(@PathVariable Long id) {
         bookService.deleteBook(id);
     }
+    @GetMapping("/book/{id}")
+    public ResponseEntity<Book> findBookById(@PathVariable long id){
+        Optional<Book> foundBook = bookService.findBook(id);
+        if(foundBook.isPresent()){
+            return new ResponseEntity<>(foundBook.get(), HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PutMapping("/book/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable long id, @RequestBody Book updatedBook){
+        try {
 
+            Book book = bookService.updatedBook(id, updatedBook);
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } catch (Exception e) {
+            //en el caso de que no encuentre devuelve not found
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
--- src/main/java/com/liberia/Library/service/BookService.java
+++ src/main/java/com/liberia/Library/service/BookService.java
@@ -5,6 +5,7 @@ import com.liberia.Library.repository.IBookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -23,5 +24,28 @@ public class BookService {
     public void deleteBook(Long id) {
         IBookRepository.deleteById(id);
 
+    }
+    public Optional<Book> findBook(Long id){
+        return IBookRepository.findById(id);
+    }
+    public Book updatedBook(Long id, Book updatedBook){
+
+        Optional<Book> foundBook = IBookRepository.findById(id);
+
+        if(foundBook.isPresent()){
+            Book existingBook = foundBook.get();
+
+
+            existingBook.setTitle(updatedBook.getTitle());
+            existingBook.setAuthor(updatedBook.getAuthor());
+            existingBook.setDescription(updatedBook.getDescription());
+            existingBook.setGenre(updatedBook.getGenre());
+            existingBook.setIsbn(updatedBook.getIsbn());
+
+
+            return IBookRepository.save(existingBook);
+        }
+
+        throw new RuntimeException("Book not found with id: " + id);
     }
 }
